de27815c6573d5d9e9f1a79bf46afb3f652ea6b7
Angelo Sanchez <asanchezcasco@alumnos.exa.unicen.edu.ar>
2020-07-02 14:20:46 +0000

    Added updateCotization on a specific client

--- controllers/cotizaciones.js
+++ controllers/cotizaciones.js
@@ -37,12 +37,54 @@ module.exports = {
 		Cliente.findOne({ _id: client, "cotizaciones._id": cotId }, { "cotizaciones.$.": 1 }, (err, client) => {
 			if (err) return res.status(500)
 				.json({ msg: `Ocurrió un error en la base de datos: ${err}` });
-			return res.status(200).json(client.cotizaciones);
+			if (!(client && client.cotizaciones && client.cotizaciones.length))
+				return res.status(404).json({ msg: 'No existe la cotización que buscas' })
+			return res.status(200).json(client.cotizaciones[0]);
 		});
-	}
+	},
 
 	//UPDATE BY ID
+	updateCotization: (req, res) => {
+		const client = req.params.cliente,
+			cotId = req.params.cotizacion,
+			updateObj = parseUpdate(req.body);
+		if (!updateObj) return res.status(400).json({ msg: "Ocurrió un error al procesar la petición" });
+		Cliente.findByIdAndUpdate(client, {
+			$set: updateObj
+		}, {
+			new: true,
+			arrayFilters: [{ "cotizacion._id": cotId }]
+		}, (err, doc) => {
+			if (err) return res.status(500)
+				.json({ msg: `Ocurrió un error en la base de datos: ${err}` });
+			return res.status(200).json(doc);
+		})
+	}
 
 	//DELETE BY ID
 
+}
+
+const parseUpdate = (body) => {
+	const upd = {};
+	if (body.margen) {
+		if (body.margen < 0 || body.margen > 1)
+			return;
+		upd["cotizaciones.$[cotizacion].margen"] = +body.margen;
+	}
+	if (body.fecha) {
+		let fecha = new Date(body.fecha);
+		if (isNaN(fecha))
+			return;
+		upd["cotizaciones.$[cotizacion].fecha"] = fecha;
+	}
+	try {
+		if (body.materiales) {
+			upd["cotizaciones.$[cotizacion].materiales"] = JSON.parse(body.materiales);
+		}
+	} catch (error) {
+		return console.error({ error });
+	}
+	console.log(JSON.stringify(upd));
+	return upd;
 }
--- routes/clientes.js
+++ routes/clientes.js
@@ -19,13 +19,8 @@ router.delete('/:cliente', clientController.deleteClient);
 router.get('/:cliente/cotizaciones', cotizationController.readCotizations);
 router.get('/:cliente/cotizaciones/:cotizacion', cotizationController.readCotization);
 router.post('/:cliente/cotizaciones', cotizationController.createCotization);
+router.put('/:cliente/cotizaciones/:cotizacion', cotizationController.updateCotization);
 
-router.put('/:cliente/cotizaciones/:cotizacion', (req, res, next) => {
-	res.status(200).send(`Hola desde ${req.baseUrl}, hemos recibido una actualizacion para la cotizacion ${req.params.cotizacion} del cliente ${req.params.cliente}<br>
-		<pre>${JSON.stringify(req.body)}</pre>`);
-	const { method, body } = req;
-	console.log({ method, body });
-});
 router.delete('/:cliente/cotizaciones/:cotizacion', (req, res, next) => {
 	res.status(200).send(`Hola desde ${req.baseUrl}, has solicitado eliminar la cotizacion con id ${req.params.cotizacion}, dirigida al cliente ${req.params.cliente}`);
 });
